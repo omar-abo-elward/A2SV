e246a3c27ae14a3ef0948c7653b06317cb0ec014
omar-abo-elward <omarward849@gmail.com>
2026-02-03 21:55:54 +0200

    Array_Subset

---
+++ Array_Subset.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubset(self, a, b):
+        freq = {}
+        for i in a:
+            freq[i]=freq.get(i,0)+1;
+        
+        for i in b:
+            if freq.get(i,0)==0:
+                return False
+            else : freq[i]=freq.get(i,0)-1
+        return True
